e2709ceafd07e48d6335cdb849ee2f5f125ed0b5
Mystery-Man-From-Ouperspace <109296003+Mystery-Man-From-Ouperspace@users.noreply.github.com>
2025-05-06 18:49:23 +0200

    Statuts pris en compte lancers dés

--- module/control-buttons.js
+++ module/control-buttons.js
@@ -1,35 +1,50 @@
+import { Macros } from "./macros.js";
+
 export function initControlButtons() {
 
   CONFIG.Canvas.layers.devastra = { layerClass: ControlsLayer, group: "primary" };
 
   Hooks.on("getSceneControlButtons", (btns) => {
-    let menu = [];
 
+    console.log("canvas.tokens = ", canvas.tokens);
+    console.log("btns = ", btns);
+    console.log("btns.tokens = ", btns.tokens);
     if (game.user.isGM) {
-      menu.push({
+      btns.tokens = {
+        name: "devastra",
+        activeTool: "gm-manager",
+        order: 999,
+        title: "Devâstra",
+        icon: "fas fa-trillium",
+        visible: true,
+        onChange: (event, active) => { Macros.showPlayersGMManagerButtons(event, active); },
+        tools: {}
+      };
+      btns.tokens.tools.gm_manager = {
         name: "gm-manager",
+        order: 0,
         title: game.i18n.localize("DEVASTRA.GMManager.Title"),
         icon: "fa fa-crosshairs-simple",
         button: true,
-        onClick: () => { game.devastra.macros.launchGMManager(); }
-      });
-
-      menu.push({
+        visible: true,
+        toggle: true,
+        onChange: (event, active) => { Macros.clickGMManagerButton(event, active); }
+      };
+      btns.tokens.tools.players_manager = {
         name: "players-manager",
+        order: 0,
         title: game.i18n.localize("DEVASTRA.PlayersManager.Title"),
         icon: "fa-solid fa-users",
         button: true,
-        onClick: () => { game.devastra.macros.launchPlayersManager(); }
-      });
+        visible: true,
+        toggle: true,
+        onChange: (event, active) => { Macros.clickPlayersManagerButton(event, active); }
+      };
+      console.log("btns.tokens = ", btns.tokens);
+      console.log("game.canvas = ", game.canvas);
 
 
-      btns.push({
-        name: "devastra",
-        title: "Devâstra",
-        icon: "fas fa-trillium",
-        layer: "devastra",
-        tools: menu
-      });
+      
     }
-  });
-}
+  })
+};
--- module/macros.js
+++ module/macros.js
@@ -1,10 +1,41 @@
 export class Macros {
     static launchGMManager = function() {
         game.devastra.gmManager.render(true);
+        console.log("Clic sur icône GM MANAGER");
+    }
+
+    clickGMManagerButton = function(event, active) {
+        console.log("Clic sur icône GM MANAGER");
+        if (event = 'onClick') {
+            if (active) {
+                game.devastra.gmManager.render(true);
+            } else {
+                game.devastra.playersManager.render(true);
+            }
+        }
     }
 
     static launchPlayersManager = function() {
         game.devastra.playersManager.render(true);
+        console.log("Clic sur icône PLAYERS MANAGER");
+    }
+    
+    static clickPlayersManagerButton = function(event, active) {
+        console.log("Clic sur icône PLAYERS MANAGER");
+        if (event = 'onClick') {
+            if (active) {
+                game.devastra.playersManager.render(true);
+            } else {
+                game.devastra.gmManager.render(true);
+            }
+        }
+    }
+
+    static showPlayersGMManagerButtons(event, active) {
+        console.log("Clic sur icône DEVÂSTRA");
+        if (event = 'onClick' && active) {
+            game.devastra.render(true);
+        }
     }
 
 }
